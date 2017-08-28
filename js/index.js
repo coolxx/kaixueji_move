b54336559386299ab25b399c821d02cb43dd8abd
henghenghahadada <xing19931122@sina.com>
2017-08-28 12:21:25 +0800

    ceshi
    asdasd

---
+++ js/index.js
@@ -0,0 +1,22 @@
+$(function(){
+	if($(window).width()<720){
+			(function (win,doc){
+				function change(){
+					doc.documentElement.style.fontSize=20*doc.documentElement.clientWidth/360+'px';
+				}
+				change();
+				win.addEventListener('resize',change,false);
+			})(window,document);
+		}
+	$(window).resize(function(){
+		if($(window).width()<720){
+			(function (win,doc){
+				function change(){
+					doc.documentElement.style.fontSize=20*doc.documentElement.clientWidth/360+'px';
+				}
+				change();
+				win.addEventListener('resize',change,false);
+			})(window,document);
+		}
+	})
+})
